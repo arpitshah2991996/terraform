692d8da0b002a361629a1db3c4d17bca4d20ebcc
Arpit Shah <arpit29996@gmail.com>
2020-05-12 22:35:35 +0530

    Correcting Script

--- test.tf
+++ test.tf
@@ -60,7 +60,6 @@ resource "aws_instance" "example" {
 
   provisioner "remote-exec" {
    inline = [
-      "sudo amazon-linux-extras enable nginx1.12",
       "sudo yum -y install nginx",
       "sudo systemctl start nginx"
     ]
